11fa250c0ce020866e9d4cb5c3520c007131a377
Collin Stasiak <collinstasiak@gmail.com>
2025-12-03 21:17:00 -0500

    Initialize new Neovim configuration
    Add basic options, keybindings, and plugins for Neovim configuration.

---
+++ nvim/new_init.lua
@@ -0,0 +1,205 @@
+--Basic Options
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.wrap = true
+vim.o.tabstop = 4
+vim.o.cursorcolumn = true
+vim.o.smartindent = false
+vim.o.termguicolors = true
+
+--Keybinds
+
+vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set({ "n", "v", "x" }, "<C-s>", [[:s/\V]], { desc = "Enter substitue mode in selection" })
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set("n", "J", "mzJ`z")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+-- greatest remap ever
+vim.keymap.set("x", "<leader>p", [["_dP]])
+
+-- next greatest remap ever : asbjornHaland
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+
+vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
+
+-- This is going to get me cancelled
+vim.keymap.set("i", "<C-c>", "<Esc>")
+
+vim.keymap.set("n", "Q", "<nop>")
+vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
+vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
+
+vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
+vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+
+vim.keymap.set("n", "<leader>vpp", "<cmd>e ~/.config/nvim/init.lua<CR>");
+
+vim.keymap.set("n", "<leader><leader>", function()
+	vim.cmd("so")
+end)
+
+
+vim.keymap.set('n', '<leader>o', ':update<CR> :source<CR>')
+
+-- Plugins
+vim.pack.add({
+	{ src = "https://github.com/vague-theme/vague.nvim" },                             -- Theme
+	{ src = "https://github.com/nvim-tree/nvim-web-devicons" },                        --Icons
+	{ src = "https://github.com/nvim-treesitter/nvim-treesitter",        version = "main" }, --Highlighting
+	{ src = "https://github.com/nvim-telescope/telescope.nvim",          version = "0.1.8" }, -- fzf
+	{ src = "https://github.com/nvim-telescope/telescope-ui-select.nvim" },            --ui
+	{ src = "https://github.com/nvim-lua/plenary.nvim" },                              -- deps
+	{ src = "https://github.com/neovim/nvim-lspconfig" },                              --lsp
+	{ src = "https://github.com/mason-org/mason.nvim" },                               --lsp
+	{ src = "https://github.com/L3MON4D3/LuaSnip" },                                   --snippet gen
+	{ src = "https://github.com/windwp/nvim-autopairs" },                              --braces
+	{ src = "https://github.com/chentoast/marks.nvim" },
+	{ src = "https://github.com/folke/trouble.nvim" },
+
+	{ src = "https://github.com/lewis6991/gitsigns.nvim" },
+
+
+})
+
+require "marks".setup {
+	builtin_marks = { "<", ">", "^" },
+}
+-- Autopair
+require("autopairs").config({
+	check_ts = true
+})
+
+--gitsigns
+require("gitsigns").setup({
+	current_line_blame = true,
+	signs = {
+		add = { text = "+" },
+		change = { text = "~" },
+		delete = { text = "_" },
+		topdelete = { text = "‾" },
+		changedelete = { text = "~" },
+	},
+	on_attach = function(bufnr)
+		local gs = package.loaded.gitsigns
+		local function map(mode, lhs, rhs, opts)
+			opts = opts or {}
+			opts.buffer = bufnr
+			vim.keymap.set(mode, lhs, rhs, opts)
+		end
+
+		map('n', ']h', gs.next_hunk)
+		map('n', '[h', gs.prev_hunk)
+		map({ 'n', 'v' }, '<leader>hs', ':Gitsigns stage_hunk<CR>')
+		map({ 'n', 'v' }, '<leader>hr', ':Gitsigns reset_hunk<CR>')
+		map('n', '<leader>hS', gs.stage_buffer)
+		map('n', '<leader>hu', gs.undo_stage_hunk)
+		map('n', '<leader>hR', gs.reset_buffer)
+		map('n', '<leader>hp', gs.preview_hunk_inline)
+		map('n', '<leader>hb', function() gs.blame_line({ full = true }) end)
+		map('n', '<leader>hd', gs.diffthis)
+	end,
+})
+
+
+--lsp
+vim.api.nvim_create_autocmd('FileType', {
+	pattern = { 'svelte', 'markdown', 'lua', 'rust', 'typst', 'typescript', 'javascript', 'c', 'cpp' },
+	callback = function() vim.treesitter.start() end,
+})
+
+
+
+-- Telescope config
+local telescope = require("telescope")
+local default_color = "vague"
+local builtin = require("telescope.builtin")
+telescope.setup({
+	defaults = {
+		preview = { treesitter = false },
+		color_devicons = true,
+		sorting_strategy = "ascending",
+		borderchars = {
+			"", -- top
+			"", -- right
+			"", -- bottom
+			"", -- left
+			"", -- top-left
+			"", -- top-right
+			"", -- bottom-right
+			"", -- bottom-left
+		},
+		path_displays = { "smart" },
+		layout_config = {
+			height = 100,
+			width = 400,
+			prompt_position = "top",
+			preview_cutoff = 40,
+		}
+	}
+})
+telescope.load_extension("ui-select")
+vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+vim.keymap.set('n', '<leader>pws', function()
+	local word = vim.fn.expand("<cword>")
+	builtin.grep_string({ search = word })
+end)
+vim.keymap.set('n', '<leader>pWs', function()
+	local word = vim.fn.expand("<cWORD>")
+	builtin.grep_string({ search = word })
+end)
+vim.keymap.set('n', '<leader>ps', function()
+	builtin.grep_string({ search = vim.fn.input("Grep > ") })
+end)
+vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
+
+
+
+require "vague".setup({ transparent = true })
+
+vim.cmd("colorscheme vague")
+
+--lsp
+
+require 'mason'.setup()
+vim.api.nvim_create_autocmd('LspAttach', {
+	group = vim.api.nvim_create_augroup('my.lsp', {}),
+	callback = function(args)
+		local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
+		if client:supports_method('textDocument/completion') then
+			-- Optional: trigger autocompletion on EVERY keypress. May be slow!
+			local chars = {}; for i = 32, 126 do table.insert(chars, string.char(i)) end
+			client.server_capabilities.completionProvider.triggerCharacters = chars
+			vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = true })
+		end
+	end,
+})
+
+vim.cmd [[set completeopt+=menuone,noselect,popup]]
+
+vim.lsp.enable({
+	"lua_ls", "cssls",
+	"rust_analyzer", "clangd",
+	"tailwindcss", "ts_ls",
+})
+
+vim.api.nvim_create_autocmd("BufWinEnter", {
+	pattern = "*.jsx,*.tsx",
+	group = vim.api.nvim_create_augroup("TS", { clear = true }),
+	callback = function()
+		vim.cmd([[set filetype=typescriptreact]])
+	end
+})
